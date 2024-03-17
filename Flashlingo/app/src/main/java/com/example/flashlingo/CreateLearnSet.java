04b362b3f455d27486bc6f710e767337a270d477
Behan Kusi <kusibehan@icloud.com>
2024-03-17 13:21:03 +0100

    Create Learnset Activities initialization

--- Flashlingo/app/src/main/java/com/example/flashlingo/CreateLearnSet.java
+++ Flashlingo/app/src/main/java/com/example/flashlingo/CreateLearnSet.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +12,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateLearnSet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button createLernSetBtn = findViewById(R.id.CreateLearnSetBtn);
-        createLernSetBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_create_learn_set);
+
+        Button createTitleBtn = findViewById(R.id.createTitleBtn);
+
+        createTitleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreateLearnSet.class);
+                Intent intent = new Intent(CreateLearnSet.this, GenerateCardsActivity.class);
+               EditText lernsetEditText = findViewById(R.id.lernSetEditText);
+               String lernSetName = lernsetEditText.getText().toString();
+                intent.putExtra("lernsetName", lernSetName);
                 startActivity(intent);
             }
         });
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- Flashlingo/app/src/main/java/com/example/flashlingo/GenerateCardsActivity.java
+++ Flashlingo/app/src/main/java/com/example/flashlingo/GenerateCardsActivity.java
@@ -11,21 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class GenerateCardsActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button createLernSetBtn = findViewById(R.id.CreateLearnSetBtn);
-        createLernSetBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_generate_cards);
+
+        String LernsetName = getIntent().getStringExtra("lernsetName");
+        Button createCardsBtn = findViewById(R.id.addBtn);
+
+        createCardsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, CreateLearnSet.class);
-                startActivity(intent);
+
             }
         });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
