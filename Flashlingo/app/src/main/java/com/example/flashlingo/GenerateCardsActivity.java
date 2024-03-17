ae3396bd88a09bc6841ea8a81f38beca42a907f4
Behan Kusi <kusibehan@icloud.com>
2024-03-17 13:37:36 +0100

    Mechanism to put store cards

--- Flashlingo/app/src/main/java/com/example/flashlingo/GenerateCardsActivity.java
+++ Flashlingo/app/src/main/java/com/example/flashlingo/GenerateCardsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+
 public class GenerateCardsActivity extends AppCompatActivity {
-    
+
+    public class Card {
+        String cardDefinition;
+        String cardWord;
+
+        public Card(String cardWord, String cardDefinition) {
+            this.cardWord = cardWord;
+            this.cardDefinition = cardDefinition;
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_generate_cards);
+        HashMap<String,Card> lernset = new HashMap<String,Card>();
 
         String LernsetName = getIntent().getStringExtra("lernsetName");
         Button createCardsBtn = findViewById(R.id.addBtn);
@@ -25,7 +40,12 @@ public class GenerateCardsActivity extends AppCompatActivity {
         createCardsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                EditText begriffEdt = findViewById(R.id.begriffEditText);
+                EditText definitionEdt = findViewById(R.id.definitionEditText);
+                String begriff = begriffEdt.getText().toString();
+                String definition = definitionEdt.getText().toString();
+                Card card = new Card(begriff,definition);
+                lernset.put(LernsetName,card);
             }
         });
 
